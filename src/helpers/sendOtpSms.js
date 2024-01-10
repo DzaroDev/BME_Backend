53bf5a9b4c0acb1a03f0135aa2830a92038d56df
Mohit Das <goodwinmohit@gmail.com>
2024-01-11 01:08:44 +0530

    update sentOtpSms()

--- src/helpers/sendOtpSms.js
+++ src/helpers/sendOtpSms.js
@@ -4,6 +4,7 @@ const debug = require('debug')('node-server:index');
 const config = require('../config');
 
 function sendOtpSms({ mobile, otpCode }) {
+  if (!config?.msg91AuthKey || !config?.msg91SenderId) return
   const sendOtp = new SendOtp(config.msg91AuthKey);
   sendOtp.send(mobile, config.msg91SenderId, otpCode, function (error, data) {
     debug('MSG91 OTP sent!', data);
